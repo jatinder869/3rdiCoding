e705216d5f69f24a8b1791cdbaeaca0d093d55ae
jatinder <jas869@usask.ca>
2022-07-28 09:52:08 -0600

    project completed with dislaying table

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useState } from 'react';
+import List from './List';
 import Login from './Login'
 
 function App() {
-  return (
-    <div >
-      <Login/>
-    </div>
-  );
+  
+  const [loginSuccess, setLoginSuccess] = useState(false)
+
+  const handleLogin = (loginValue) => {
+    setLoginSuccess(loginValue)
+  }
+
+  if (loginSuccess){
+    return (
+      <div>
+        <List/>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Login onLogin={handleLogin}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,13 +1,43 @@
-const Login = ()=>{
+import axios from 'axios'
+import { useState } from 'react'
+import './login.css'
+import { motion } from 'framer-motion'
+
+const Login = (props) => {
+    const {onLogin} = props    
+    const [loginInfo, setLoginInfo] = useState({
+        email: undefined,
+        password: undefined
+    })
+    const [successLogin, setSuccesLogin] = useState()
+
+
+    const sendLoginInfo = () => {
+        axios.post("https://www.savemybills.com.au/api/loginCheck", loginInfo)
+            .then(res => {
+                setSuccesLogin(res.data.flag)
+                onLogin(res.data.flag)
+            })
+    }
+
     return (
-        <div>
-            <form onSubmit={e => e.preventDefault()}>
+       
+        <div className='login'>
+            <div className='header'> Login to Access Retailers </div>
+            <motion.form 
+            animate={{rotate:[0,10,-10,10,-10,0]}}
+            transition={{repeat:1,duration:1}}
+            className="login-form" onSubmit={e => {
+                    e.preventDefault()
+                    sendLoginInfo()
+                }}>
                 <label htmlFor="email-id">Email</label>
-                <input id="email-id" name="email-id" placeholder="abc@email.com"/>
+                <input id="email-id" name="email-id" placeholder="abc@email.com" onChange={event => setLoginInfo(prevState => ({...prevState, email:event.target.value}))} required/>
                 <label htmlFor="password">Password</label>
-                <input name="password" id="password" placeholder="password" />
+                <input name="password" id="password" placeholder="password" type="password" onChange={event => setLoginInfo( prevState => ({...prevState, password:event.target.value}))} required />
+                {successLogin === false && <p className='login-failed'>Login Failed</p>}
                 <button type="submit">Login</button>
-            </form>
+            </motion.form>
         </div>
     )
 }
